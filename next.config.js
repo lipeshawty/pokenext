c66e34b59a3746b11b94d85e11a50c6a21ce5a7f
lipeshawty <lipebroken@gmail.com>
2022-12-01 21:08:33 -0300

    correções

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['raw.githubusercontent.com']
-  }
+  },
+  experimental: {
+    largePageDataBytes: 800 * 1000,
+  },
 }
 
 module.exports = nextConfig
